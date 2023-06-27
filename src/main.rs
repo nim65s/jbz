225f31b0974a4fe20e9e6aed5061b5f1fbe25f24
Guilhem Saurel <guilhem.saurel@laas.fr>
2023-06-28 01:23:19 +0200

    Commands wont work in wasi :(
    ref. https://github.com/casey/just/issues/365#issuecomment-1610357375

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,27 @@
+use std::process::Command;
 use zellij_tile::prelude::*;
 
 #[derive(Default)]
 struct State {
     build_runs: usize,
+    commands: Vec<String>,
 }
 
 register_plugin!(State);
 
+fn just_commands() -> Vec<String> {
+    let output = Command::new("just").arg("-l").output().unwrap();
+    String::from_utf8_lossy(&output.stdout)
+        .split('\n')
+        .skip(1)
+        .map(|s| s.trim().to_string())
+        .collect()
+}
+
 impl ZellijPlugin for State {
     fn load(&mut self) {
         subscribe(&[EventType::Key]);
+        self.commands = just_commands()
     }
 
     fn update(&mut self, event: Event) -> bool {
@@ -24,6 +36,11 @@ impl ZellijPlugin for State {
     fn render(&mut self, rows: usize, cols: usize) {
         println!();
         println!("I have {rows} rows and {cols} columns");
+        println!();
+        println!("here are the available commands:");
+        for cmd in &self.commands {
+            println!(" - {cmd}");
+        }
         println!();
         if self.build_runs > 0 {
             println!("Ran tests {} times!", self.build_runs);
